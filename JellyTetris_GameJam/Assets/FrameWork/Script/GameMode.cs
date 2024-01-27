bfaa08d2fcb7ead9101c23a03a3492d3c069291c
chickenbuger <gksdidxornjs@naver.com>
2024-01-27 11:37:24 +0900

    Update GameMode.cs

--- JellyTetris_GameJam/Assets/FrameWork/Script/GameMode.cs
+++ JellyTetris_GameJam/Assets/FrameWork/Script/GameMode.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameMode : MonoBehaviour
@@ -29,6 +31,35 @@ public class GameMode : MonoBehaviour
         
     }
 
+    public void RequestDeleteGridBoardLineCheck(Vector3[] Positions)
+    {
+        float[] yDatas = new float[4];
+        for(int i = 0; i < 4; i++)
+        {
+            yDatas[i] = Positions[i].y;
+        }
+        float[] uniqueArray = RemoveDuplicates(yDatas);
+        int[] iy = CastPositionToGridIndex(uniqueArray);
+        Array.Sort(iy,(a,b)=>b.CompareTo(a));
+        bool[] bDatas = DeleteCheckGridLines(iy);
+        DeleteGridLine(iy, bDatas);
+
+        RequestSpawnerMinoActorToSpawner();
+    }
+
+    private int[] CastPositionToGridIndex(float[] Positions)
+    {
+        int[] GridIndex = new int[Positions.Length];
+
+        int i = 0;
+        foreach(float index in Positions)
+        {
+            GridIndex[i] = (int)((index + 5.5f) * 2.0f);
+        }    
+
+        return GridIndex;
+    }
+
     private void RequestSpawnerMinoActorToSpawner()
     {
         ObjectSpawner.GetComponent<Spawner>().RequestCreateMino();
@@ -37,6 +68,22 @@ public class GameMode : MonoBehaviour
     private void CreateObjectSpawner()
     {
         Instantiate(ObjectSpawner, transform.position, Quaternion.identity);
+    }
+    private float[] RemoveDuplicates(float[] array)
+    {
+        List<float> uniqueList = new List<float>();
+        HashSet<float> seenValues = new HashSet<float>();
+
+        foreach (float value in array)
+        {
+            // HashSet을 사용하여 중복값을 체크하고 중복이 아닌 경우에만 리스트에 추가
+            if (seenValues.Add(value))
+            {
+                uniqueList.Add(value);
+            }
+        }
+
+        return uniqueList.ToArray();
     }
 
     private void InitGridBoard()
@@ -49,6 +96,39 @@ public class GameMode : MonoBehaviour
                 //Init Wall Setting
                 if (x == 0 || x == 11) GridBoard[y, x].z = 3.0f;
                 if (y == 0) GridBoard[y, x].z = 3.0f;
+            }
+        }
+    }
+
+    private bool[] DeleteCheckGridLines(int[] _Lines)
+    {
+        bool[] bDatas = new bool[4];
+        for (int i = 0; i < _Lines.Length; i++)
+        {
+            if (_Lines[i] < 1 || _Lines[i] > 11) { bDatas[i] = false; continue; }
+            for (int index = 1; index < 11; index++)
+            {
+                if (_Lines[index] == 0) { bDatas[i] = false; continue; }
+            }
+            bDatas[i] = true;
+        }
+
+        return bDatas;
+    }
+
+    private void DeleteGridLine(int[] _iLines, bool[] _bLines)
+    {
+        for(int i=0;i< _iLines.Length;i++)
+        {
+            if (_bLines[i])
+            {
+                for(int y = _iLines[i] + 1; y < 22; y++)
+                {
+                    for(int x = 1; x < 11; x++)
+                    {
+                        GridBoard[y - 1, x] = GridBoard[y, x];
+                    }
+                }
             }
         }
     }
